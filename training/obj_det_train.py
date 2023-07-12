bb8bb4e548da2e0903f7df84103a85733f2c0e58
Eric Escareno <ericescareno72@gmail.com>
2023-07-12 16:41:55 -0700

    Format to meet flake8 and pylint requirements

--- training/obj_det_train.py
+++ training/obj_det_train.py
@@ -1,19 +1,15 @@
 """Retrain object detection model and export to tflite model
 """
-import numpy as np
-import os
 import argparse
+import tensorflow as tf
+from absl import logging
 
-from tflite_model_maker.config import QuantizationConfig
-from tflite_model_maker.config import ExportFormat
 from tflite_model_maker import model_spec
 from tflite_model_maker import object_detector
 
-import tensorflow as tf
 assert tf.__version__.startswith('2')
 
 tf.get_logger().setLevel('ERROR')
-from absl import logging
 logging.set_verbosity(logging.ERROR)
 
 # Set object detection model architecture
@@ -21,20 +17,25 @@ spec = model_spec.get('efficientdet_lite0')
 
 # Get input csv file as command line argument
 parser = argparse.ArgumentParser()
-parser.add_argument("input_file", type=str, help="filepath for the CSV input file")
+parser.add_argument("input_file", type=str,
+                    help="filepath for the CSV input file")
 args = parser.parse_args()
 
 # Load dataset
-train_data, validation_data, test_data = object_detector.DataLoader.from_csv(args.input_file)
+# pylint: disable=unbalanced-tuple-unpacking
+train_data, validation_data, test_data = \
+    object_detector.DataLoader.from_csv(args.input_file)
 
 # Train TF model with training data
-model = object_detector.create(train_data, model_spec=spec, batch_size=8, train_whole_model=True, validation_data=validation_data)
+model = object_detector.create(train_data, model_spec=spec,
+                               batch_size=8, train_whole_model=True,
+                               validation_data=validation_data)
 
 # Evaluate model with test data
-model.evaluate(test_data)
+print(model.evaluate(test_data))
 
 # Export as tflite model
 model.export(export_dir='.')
 
 # Evaluate tflite model
-model.evaluate_tflite('model.tflite', test_data)
+print(model.evaluate_tflite('model.tflite', test_data))
