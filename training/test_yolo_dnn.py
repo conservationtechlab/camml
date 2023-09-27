90685742107c3dd1b53e953a0d3275cefb370768
Eric Escareno <ericescareno72@gmail.com>
2023-09-27 10:50:04 -0700

    Format to meet Flake8 requirements

--- training/test_yolo_dnn.py
+++ training/test_yolo_dnn.py
@@ -21,9 +21,9 @@ model_name = args.model_file[:-2]
 model_onnx = model_name + 'onnx'
 
 # If onnx model file does not exist create it
-if os.path.exists(model_onnx) == False:
+if not os.path.exists(model_onnx):
     # Export the model to ONNX format
-    model.export(format="onnx",imgsz=[640,640], opset=12)
+    model.export(format="onnx", imgsz=[640, 640], opset=12)
 
 # Replace onnx filename with model_file + .onnx
 net = cv2.dnn.readNet(model_onnx)
@@ -46,7 +46,8 @@ period = args.image_file.rfind('.')
 image_type = args.image_file[period:]
 
 image = cv2.imread(args.image_file)
-blob = cv2.dnn.blobFromImage(image, 1/255.0, (INPUT_WIDTH, INPUT_HEIGHT), swapRB=True, crop=False)
+blob = cv2.dnn.blobFromImage(image, 1/255.0, (INPUT_WIDTH, INPUT_HEIGHT),
+                             swapRB=True, crop=False)
 net.setInput(blob)
 preds = net.forward()
 preds = preds.transpose((0, 2, 1))
@@ -63,27 +64,28 @@ rows = preds[0].shape[0]
 for i in range(rows):
     row = preds[0][i]
     conf = row[4]
-    
+
     classes_score = row[4:]
-    _,_,_, max_idx = cv2.minMaxLoc(classes_score)
+    _, _, _, max_idx = cv2.minMaxLoc(classes_score)
     class_id = max_idx[1]
     if (classes_score[class_id] > .25):
         confs.append(conf)
         label = CLASSES_YOLO[int(class_id)]
         class_ids.append(label)
-        
-        #extract boxes
-        x, y, w, h = row[0].item(), row[1].item(), row[2].item(), row[3].item() 
+
+        # Extract boxes
+        x, y, w, h = row[0].item(), row[1].item(), \
+            row[2].item(), row[3].item()
         left = int((x - 0.5 * w) * x_factor)
         top = int((y - 0.5 * h) * y_factor)
         width = int(w * x_factor)
         height = int(h * y_factor)
         box = np.array([left, top, width, height])
         boxes.append(box)
-        
+
 r_class_ids, r_confs, r_boxes = list(), list(), list()
 
-indexes = cv2.dnn.NMSBoxes(boxes, confs, 0.25, 0.45) 
+indexes = cv2.dnn.NMSBoxes(boxes, confs, 0.25, 0.45)
 for i in indexes:
     r_class_ids.append(class_ids[i])
     r_confs.append(confs[i])
@@ -95,8 +97,9 @@ for i in indexes:
     top = box[1]
     width = box[2]
     height = box[3]
-    
-    cv2.rectangle(image, (left, top), (left + width, top + height), (0,255,0), 3)
+
+    cv2.rectangle(image, (left, top), (left + width, top + height),
+                  (0, 255, 0), 3)
 img = Image.fromarray(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
 img.show()
 
